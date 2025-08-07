9efcd238a5344a60f30831f01510350cab44d3a7
KovendanRaman <2566527@students.wits.ac.za>
2025-08-07 15:03:56 +0200

    Initial Auth0 setup and frontend working

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css'
 import App from './App.tsx'
+const root = createRoot(document.getElementById('root')!);
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+root.render(
+<Auth0Provider
+    domain="dev-6mvzvr3totwc6rkd.us.auth0.com"
+    clientId="nM3L0iuU12uOqSfucxxThUZFtt8xbfGP"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </StrictMode>,
-)
+  </Auth0Provider>,
+);
