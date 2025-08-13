8b90c9ffcb1fade463ed1bb1e397224ad9a8edfe
Abdullah2309318 <2309318@student.wits.ac.za>
2025-08-13 21:18:26 +0200

    app.module & app.controller edits for deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,17 @@
+// backend/src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Add this section for CORS
+  const frontendURL = process.env.FRONTEND_URL;
+  app.enableCors({
+    origin: [frontendURL],
+  });
+
+  // Your port logic is correct
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
